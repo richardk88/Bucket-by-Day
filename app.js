02e051106aaac98f0cfc6ec92999f8739acde06c
HunterMcAfee <huntmac10@gmail.com>
2017-07-28 15:03:16 -0400

    Restructured app.js for readability

--- app.js
+++ app.js
@@ -6,14 +6,10 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-require('dotenv').config();
-
-mongoose.connect(process.env.MONGODB_URI); 
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
+
+require('dotenv').config();
+mongoose.connect(process.env.MONGODB_URI); 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,7 +23,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//requiring routes
+var index = require('./routes/index');
 app.use('/', index);
+
+var users = require('./routes/users');
 app.use('/users', users);
 
 // catch 404 and forward to error handler
