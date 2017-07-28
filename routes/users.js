f0e41bee51ce23334bff5f629a3c469d9a6e5f0c
HunterMcAfee <huntmac10@gmail.com>
2017-07-28 16:02:10 -0400

    Added user route folder and created user index view

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../db/schema/user.js');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  User.find({})
+    .then( (users) => {
+      res.render('../views/user/index', {
+        users: users
+      });
+    })
+    .catch( (error) => {
+      console.log('Error retrieving users from db');
+      console.log(error);
+    })
+  
 });
 
 module.exports = router;
