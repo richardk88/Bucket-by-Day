635348b7697cb82aeb8ed362614a4e05f4876b18
richardk88 <richardk88@hotmail.com>
2017-07-30 18:47:01 -0400

    added more info to seeds

--- db/seeds.js
+++ db/seeds.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect('mongodb://localhost/bucket-by-day');
-mongoose.connect(process.env.MONGODB_URI); 
+mongoose.connect('mongodb://localhost/bucket-by-day');
+// mongoose.connect(process.env.MONGODB_URI); 
 
 const User = require('../models/user');
 const BucketList = require('../models/bucketList');
-const Activities = require('../models/activities');
+const Activities = require('../models/activityList');
 
 mongoose.Promise = global.Promise;
 
@@ -52,13 +52,87 @@ const cydActivities3 = new Activities (
     }
 )
 
-const cydBucketList = new BucketList (
+const cydBucketList1 = new BucketList (
     {
         name: 'Debauchary',
         description: 'When you start out real chill and classy-like but end it like G.',
         theme: 'Wavy Nights',
         totalCost: 0,
         activities: [cydActivities1, cydActivities2, cydActivities3]
+    }
+)
+const cydActivities4 = new Activities (
+   {
+       name: 'Fur Bus',
+       description: 'Cause you should not drink and drive, kids.',
+       location: '1199 Atlanta Industrial Dr, Marietta, GA 30066',
+       duration: '4+ hrs',
+       link: 'http://www.furbus.com/',
+       price: 500
+   }
+)
+
+const cydActivities5 = new Activities (
+   {
+       name: 'The Cheetah',
+       description: 'Bc the chicken wings are hittin\' and so are the girls',
+       location: '887 Spring St NW, Atlanta, GA 30308',
+       duration: '4+ hrs',
+       link: 'https://www.thecheetah.com/',
+       price: 1000
+   }
+)
+
+const cydBucketList2 = new BucketList (
+    {
+        name: 'Debauchary2',
+        description: 'When you start out real chill and classy-like but end it like G.',
+        theme: 'Wavy Nights',
+        totalCost: 0,
+        activities: [cydActivities4, cydActivities5]
+    }
+)
+
+const cydActivities6 = new Activities (
+   {
+       name: 'Illegal Food',
+       description: 'Because its criminal',
+       location: '1044 Greenwood Ave NE, Atlanta, GA 30306',
+       duration: '2 hrs',
+       link: 'http://www.illegalfoodatlanta.com/',
+       price: 20
+   }
+)
+
+const cydActivities7 = new Activities (
+   {
+       name: 'Plaza Theatre',
+       description: 'Catch a flick',
+       location: '1049 Ponce De Leon Ave NE, Atlanta, GA 30306',
+       duration: '2 hrs',
+       link: 'http://plazaatlanta.com/',
+       price: 20
+   }
+)
+
+const cydActivities8 = new Activities (
+   {
+       name: 'Blind Willies',
+       description: 'Because everyone should go here before they die.',
+       location: '828 North Highland Avenue Northeast, Atlanta, GA 30306',
+       duration: '3 hrs',
+       link: 'http://www.blindwilliesblues.com/',
+       price: 50
+   }
+)
+
+const cydBucketList3 = new BucketList (
+    {
+        name: 'Debauchary3',
+        description: 'When you start out real chill and classy-like but end it like G.',
+        theme: 'Wavy Nights',
+        totalCost: 0,
+        activities: [cydActivities6, cydActivities7, cydActivities8]
     }
 )
 
@@ -69,65 +143,107 @@ const cydUser = new User (
         userName: 'CydVicious',
         email: 'cyd@yahoo.com',
         img: '',
-        bucketLists: [cydBucketList]
+        bucketLists: [cydBucketList1, cydBucketList2, cydBucketList3]
     }
 )
 
-const hunterActivities1 = new Activities (
+const jeffreyActivities1 = new Activities (
     {
         name: 'Opera Night Club',
-        description: 'Go see the current DJ',
-        location: '1218 Peachtree Street',
+        description: 'Go see the the current DJ',
+        location: '1150 Crescent Ave NE, Atlanta, GA 30309',
         duration: '2 hr',
-        link: 'http://www.operaATL.com',
+        link: 'http://operaatlanta.com/',
         price: 50,
     }
 )
 
-const hunterActivities2 = new Activities (
+const jeffreyActivities2 = new Activities (
     {
         name: 'Georgia Beer Garden',
         description: 'Go grab some beers after the show for night cap',
-        location: '1932 Edgewood Street',
+        location: '420 Edgewood Avenue, Atlanta, GA 30312',
         duration: '2 hr',
         link: 'http://www.georgiabeergarden.com',
         price: 20,
     }
 )
 
-const hunterActivities3 = new Activities (
+const jeffreyActivities3 = new Activities (
     {
         name: 'Waffle House',
-        description: "Grab some food cause you are drunk",
-        location: '1218 Peachtree Street',
+        description: "Go get some some food after hanging with friends at the Georgia Beer Garden",
+        location: '66 5th St NW, Atlanta, GA 30308',
         duration: '45 min',
         link: 'http://www.wafflehouse.com',
         price: 20,
     }
 )
 
-const hunterBucketList = new BucketList (
+const jeffreyBucketList1 = new BucketList (
     {
         name: 'Night Out',
-        description: 'The most lit night ever',
+        description: 'This is a night for those looking to experience the Atlanta night life.',
         theme: 'Wavy Nights',
         totalCost: 90,
-        activities: [hunterActivities1, hunterActivities2, hunterActivities3]
+        activities: [jeffreyActivities1, jeffreyActivities2, jeffreyActivities3]
     }
 )
-
-const hunterUser = new User (
+const jeffreyActivities4 = new Activities (
     {
-        firstName: 'Hunter',
-        lastName: 'McAfee',
-        userName: 'Sono Ryoushi',
-        email: 'huntmac@yahoo.com',
-        img: '',
-        bucketLists:[hunterBucketList]
+        name: 'Opera Night Club',
+        description: 'Go see the the current DJ',
+        location: '1150 Crescent Ave NE, Atlanta, GA 30309',
+        duration: '2 hr',
+        link: 'http://operaatlanta.com/',
+        price: 50,
     }
 )
 
-const richardActivities1 = new Activities (
+const jeffreyActivities5 = new Activities (
+    {
+        name: 'Georgia Beer Garden',
+        description: 'Go grab some beers after the show for night cap',
+        location: '420 Edgewood Avenue, Atlanta, GA 30312',
+        duration: '2 hr',
+        link: 'http://www.georgiabeergarden.com',
+        price: 20,
+    }
+)
+
+const jeffreyActivities6 = new Activities (
+    {
+        name: 'Waffle House',
+        description: "Go get some some food after hanging with friends at the Georgia Beer Garden",
+        location: '66 5th St NW, Atlanta, GA 30308',
+        duration: '45 min',
+        link: 'http://www.wafflehouse.com',
+        price: 20,
+    }
+)
+
+const jeffreyBucketList2 = new BucketList (
+    {
+        name: 'Night Life',
+        description: 'This is a night for those looking to experience the Atlanta night life.',
+        theme: 'Wavy Nights',
+        totalCost: 90,
+        activities: [jeffreyActivities4, jeffreyActivities5, jeffreyActivities6]
+    }
+)
+
+const jeffreyUser = new User (
+    {
+        firstName: 'Jeffrey',
+        lastName: 'Dawson',
+        userName: 'Jdawson',
+        email: 'Jdawg@yahoo.com',
+        img: 'https://static.pexels.com/photos/91227/pexels-photo-91227.jpeg',
+        bucketLists:[jeffreyBucketList1, jeffreyBucketList2]
+    }
+)
+
+const murphyActivities1 = new Activities (
     {
         name: 'Biking on the Belt',
         description: 'Rent a bike for a few hours and ride it on the beltline.',
@@ -138,7 +254,7 @@ const richardActivities1 = new Activities (
     }
 )
 
-const richardActivities2 = new Activities (
+const murphyActivities2 = new Activities (
     {
         name: 'Hiking',
         description: 'Hike up Stone Mountain.',
@@ -149,24 +265,56 @@ const richardActivities2 = new Activities (
     }
 )
 
-const richardBucketList = new BucketList (
+const murphyBucketList1 = new BucketList (
     {
         name: 'Outdoors',
-        description: 'Looking for some fresh air? Here\'s a list of things to do outside.',
+        description: 'Looking for some fresh air? Here\'s a list of outdoor activities.',
         theme: '',
         totalCost: 30,
-        activities: [richardActivities1, richardActivities2]
+        activities: [murphyActivities1, murphyActivities2]
     }
 )
 
-const richardUser = new User (
+const murphyActivities3 = new Activities (
     {
-        firstName: 'Richard',
-        lastName: 'Kim',
-        userName: 'RichKing25',
-        email: 'richardk@hotmail.com',
-        img: '',
-        bucketLists: [richardBucketList]
+        name: 'Biking on the Belt',
+        description: 'Rent a bike for a few hours and ride it on the beltline.',
+        location: '123 MLK Drive, Atlanta, GA 30303',
+        duration: '1.5 hr',
+        link: 'https://beltline.org/',
+        price: 10
+    }
+)
+
+const murphyActivities4 = new Activities (
+    {
+        name: 'Hiking',
+        description: 'Hike up Stone Mountain.',
+        location: '1000 Robert E. Lee Blvd Stone Mountain, GA 30083',
+        duration: '2 hr',
+        link: 'http://www.stonemountainpark.com/',
+        price: 20
+    }
+)
+
+const murphyBucketList2 = new BucketList (
+    {
+        name: "Pandora's Box",
+        description: "Dare to mix things up and jump into the unknown? Here's a list of random things to do for a random day.",
+        theme: '',
+        totalCost: 30,
+        activities: [murphyActivities3, murphyActivities4]
+    }
+)
+
+const murphyUser = new User (
+    {
+        firstName: 'Murphy',
+        lastName: 'Jackson',
+        userName: 'MJ300',
+        email: 'mj3000@hotmail.com',
+        img: 'https://images.pexels.com/photos/25758/pexels-photo.jpg?w=1260&h=750&auto=compress&cs=tinysrgb',
+        bucketLists: [murphyBucketList1, murphyBucketList2]
     }
 )
 
@@ -175,14 +323,14 @@ cydUser.save((err) => {
     console.log('Cyd was created');
 });
 
-hunterUser.save((err) => {
+jeffreyUser.save((err) => {
     if (err) console.log(err);
-    console.log('Hunter was created');
+    console.log('Jeffrey was created');
 });
 
-richardUser.save((err) => {
+murphyUser.save((err) => {
     if (err) console.log(err);
-    console.log('Richard was created');
+    console.log('Murphy was created');
 });
 
 mongoose.connection.close();
