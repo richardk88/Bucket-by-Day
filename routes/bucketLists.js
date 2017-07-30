f2efa041e3ab7b1422d7ce0506bb2c76e2c0101d
HunterMcAfee <huntmac10@gmail.com>
2017-07-29 20:25:00 -0400

    Created new route and view for bucketlist route

--- routes/bucketLists.js
+++ routes/bucketLists.js
@@ -21,6 +21,36 @@ router.get('/', (req, res, next) => {
     });
 });
 
+router.get('/new', (req, res) => {
+    const userId = req.params.userId;
+    res.render('../views/bucketList/new', {
+        userId: userId
+    })
+});
+
+router.post('/', (req, res) => {
+    const userId = req.params.userId;
+    const bucketListInfo = req.body;
+    User.findById(userId)
+        .then( (user) => {
+            const newBucketList = new BucketLists (bucketListInfo)
+            user.bucketLists.push(newBucketList);
+            user.save();
+
+            console.log(`Bucketlist was created for ${userId}`);
+            return res.render('../views/bucketList/show', {
+                userId: userId,
+                bucketListName: newBucketList.name,
+                bucketListDescription: newBucketList.description,
+                activities: newBucketList.activities
+            })
+        })
+        .catch((error) => {
+            console.log(error);
+            console.log('Failed to create bucketlist');
+        })
+});
+
 router.get('/:bucketId', (req, res) => {
     const userId = req.params.userId;
     const bucketId = req.params.bucketId;
@@ -29,7 +59,14 @@ router.get('/:bucketId', (req, res) => {
             const foundBucket = user.bucketLists.find((bucket) => {
                 return bucket.id === bucketId
             });
-            res.send(foundBucket);
+            res.render('../views/bucketList/show', {
+                userId: userId,
+                bucketId: bucketId,
+                bucketListName: foundBucket.name,
+                bucketListDescription: foundBucket.description,
+                bucketListTotalCost: foundBucket.totalCost,
+                activities: foundBucket.activities
+            });
         })
         .catch((error) => {
             console.log('Failed to find bucketlist');
