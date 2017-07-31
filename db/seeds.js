22363295551a261e020edbfee2fbe3757bbaebb9
richardk88 <richardk88@hotmail.com>
2017-07-31 18:24:34 -0400

    testing heroku

--- db/seeds.js
+++ db/seeds.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/bucket-by-day');
-// mongoose.connect(process.env.MONGODB_URI); 
+require('dotenv').config();
+// mongoose.connect('mongodb://localhost/bucket-by-day');
+mongoose.connect(process.env.MONGODB_URI); 
 
 const User = require('../models/user');
 const BucketList = require('../models/bucketList');
